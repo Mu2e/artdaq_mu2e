ff0268b7dbe746abd1f779df71d1c4a2d54ab4b3
John Freeman <jcfree@fnal.gov>
2014-12-16 18:16:09 -0600

    Add buildinfo functionality to the package

---
+++ mu2e-artdaq/ArtModules/Mu2eArtdaqBuildInfo_module.cc
@@ -0,0 +1,15 @@
+#include "artdaq-core/ArtModules/BuildInfo_module.hh" 
+
+#include "artdaq/BuildInfo/GetPackageBuildInfo.hh" 
+#include "artdaq-core/BuildInfo/GetPackageBuildInfo.hh" 
+#include "mu2e-artdaq/BuildInfo/GetPackageBuildInfo.hh" 
+
+#include <string>
+
+namespace mu2e {
+
+  static std::string instanceName = "Mu2eArtdaqBuildInfo";
+  typedef artdaq::BuildInfo< &instanceName, artdaqcore::GetPackageBuildInfo, artdaq::GetPackageBuildInfo, mu2e::GetPackageBuildInfo> Mu2eArtdaqBuildInfo;
+
+  DEFINE_ART_MODULE(Mu2eArtdaqBuildInfo)
+}
