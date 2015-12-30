5342e6889386731b853e0d84ad9c31ab71eae4cc
MU2E <mu2edaq@mu2edaq01.fnal.gov>
2015-12-30 15:02:42 -0600

    Use the full Pilot system (for one node, use the feature/oneNodeSystem branch)

--- tools/startMu2ePilotSystem.sh
+++ tools/startMu2ePilotSystem.sh
@@ -9,10 +9,10 @@ tempFile="$logroot/pmtConfig.$$"
 echo "Currently the Pilot System is a 5x50x2 ARTDAQ configuration"
 
 echo "BoardReaderMain mu2edaq04.fnal.gov ${MU2EARTDAQ_BR_PORT[0]}" >> $tempFile
-# echo "BoardReaderMain mu2edaq05.fnal.gov ${MU2EARTDAQ_BR_PORT[1]}" >> $tempFile
-# echo "BoardReaderMain mu2edaq06.fnal.gov ${MU2EARTDAQ_BR_PORT[2]}" >> $tempFile
-# echo "BoardReaderMain mu2edaq07.fnal.gov ${MU2EARTDAQ_BR_PORT[3]}" >> $tempFile
-# echo "BoardReaderMain mu2edaq08.fnal.gov ${MU2EARTDAQ_BR_PORT[4]}" >> $tempFile
+echo "BoardReaderMain mu2edaq05.fnal.gov ${MU2EARTDAQ_BR_PORT[1]}" >> $tempFile
+echo "BoardReaderMain mu2edaq06.fnal.gov ${MU2EARTDAQ_BR_PORT[2]}" >> $tempFile
+echo "BoardReaderMain mu2edaq07.fnal.gov ${MU2EARTDAQ_BR_PORT[3]}" >> $tempFile
+echo "BoardReaderMain mu2edaq08.fnal.gov ${MU2EARTDAQ_BR_PORT[4]}" >> $tempFile
 echo "EventBuilderMain mu2edaq04.fnal.gov ${MU2EARTDAQ_EB_PORT[0]}" >> $tempFile
 echo "EventBuilderMain mu2edaq04.fnal.gov ${MU2EARTDAQ_EB_PORT[1]}" >> $tempFile
 echo "EventBuilderMain mu2edaq04.fnal.gov ${MU2EARTDAQ_EB_PORT[2]}" >> $tempFile
@@ -23,46 +23,46 @@ echo "EventBuilderMain mu2edaq04.fnal.gov ${MU2EARTDAQ_EB_PORT[6]}" >> $tempFile
 echo "EventBuilderMain mu2edaq04.fnal.gov ${MU2EARTDAQ_EB_PORT[7]}" >> $tempFile
 echo "EventBuilderMain mu2edaq04.fnal.gov ${MU2EARTDAQ_EB_PORT[8]}" >> $tempFile
 echo "EventBuilderMain mu2edaq04.fnal.gov ${MU2EARTDAQ_EB_PORT[9]}" >> $tempFile
-# echo "EventBuilderMain mu2edaq05.fnal.gov ${MU2EARTDAQ_EB_PORT[10]}" >> $tempFile
-# echo "EventBuilderMain mu2edaq05.fnal.gov ${MU2EARTDAQ_EB_PORT[11]}" >> $tempFile
-# echo "EventBuilderMain mu2edaq05.fnal.gov ${MU2EARTDAQ_EB_PORT[12]}" >> $tempFile
-# echo "EventBuilderMain mu2edaq05.fnal.gov ${MU2EARTDAQ_EB_PORT[13]}" >> $tempFile
-# echo "EventBuilderMain mu2edaq05.fnal.gov ${MU2EARTDAQ_EB_PORT[14]}" >> $tempFile
-# echo "EventBuilderMain mu2edaq05.fnal.gov ${MU2EARTDAQ_EB_PORT[15]}" >> $tempFile
-# echo "EventBuilderMain mu2edaq05.fnal.gov ${MU2EARTDAQ_EB_PORT[16]}" >> $tempFile
-# echo "EventBuilderMain mu2edaq05.fnal.gov ${MU2EARTDAQ_EB_PORT[17]}" >> $tempFile
-# echo "EventBuilderMain mu2edaq05.fnal.gov ${MU2EARTDAQ_EB_PORT[18]}" >> $tempFile
-# echo "EventBuilderMain mu2edaq05.fnal.gov ${MU2EARTDAQ_EB_PORT[19]}" >> $tempFile
-# echo "EventBuilderMain mu2edaq06.fnal.gov ${MU2EARTDAQ_EB_PORT[20]}" >> $tempFile
-# echo "EventBuilderMain mu2edaq06.fnal.gov ${MU2EARTDAQ_EB_PORT[21]}" >> $tempFile
-# echo "EventBuilderMain mu2edaq06.fnal.gov ${MU2EARTDAQ_EB_PORT[22]}" >> $tempFile
-# echo "EventBuilderMain mu2edaq06.fnal.gov ${MU2EARTDAQ_EB_PORT[23]}" >> $tempFile
-# echo "EventBuilderMain mu2edaq06.fnal.gov ${MU2EARTDAQ_EB_PORT[24]}" >> $tempFile
-# echo "EventBuilderMain mu2edaq06.fnal.gov ${MU2EARTDAQ_EB_PORT[25]}" >> $tempFile
-# echo "EventBuilderMain mu2edaq06.fnal.gov ${MU2EARTDAQ_EB_PORT[26]}" >> $tempFile
-# echo "EventBuilderMain mu2edaq06.fnal.gov ${MU2EARTDAQ_EB_PORT[27]}" >> $tempFile
-# echo "EventBuilderMain mu2edaq06.fnal.gov ${MU2EARTDAQ_EB_PORT[28]}" >> $tempFile
-# echo "EventBuilderMain mu2edaq06.fnal.gov ${MU2EARTDAQ_EB_PORT[29]}" >> $tempFile
-# echo "EventBuilderMain mu2edaq07.fnal.gov ${MU2EARTDAQ_EB_PORT[30]}" >> $tempFile
-# echo "EventBuilderMain mu2edaq07.fnal.gov ${MU2EARTDAQ_EB_PORT[31]}" >> $tempFile
-# echo "EventBuilderMain mu2edaq07.fnal.gov ${MU2EARTDAQ_EB_PORT[32]}" >> $tempFile
-# echo "EventBuilderMain mu2edaq07.fnal.gov ${MU2EARTDAQ_EB_PORT[33]}" >> $tempFile
-# echo "EventBuilderMain mu2edaq07.fnal.gov ${MU2EARTDAQ_EB_PORT[34]}" >> $tempFile
-# echo "EventBuilderMain mu2edaq07.fnal.gov ${MU2EARTDAQ_EB_PORT[35]}" >> $tempFile
-# echo "EventBuilderMain mu2edaq07.fnal.gov ${MU2EARTDAQ_EB_PORT[36]}" >> $tempFile
-# echo "EventBuilderMain mu2edaq07.fnal.gov ${MU2EARTDAQ_EB_PORT[37]}" >> $tempFile
-# echo "EventBuilderMain mu2edaq07.fnal.gov ${MU2EARTDAQ_EB_PORT[38]}" >> $tempFile
-# echo "EventBuilderMain mu2edaq07.fnal.gov ${MU2EARTDAQ_EB_PORT[39]}" >> $tempFile
-# echo "EventBuilderMain mu2edaq08.fnal.gov ${MU2EARTDAQ_EB_PORT[40]}" >> $tempFile
-# echo "EventBuilderMain mu2edaq08.fnal.gov ${MU2EARTDAQ_EB_PORT[41]}" >> $tempFile
-# echo "EventBuilderMain mu2edaq08.fnal.gov ${MU2EARTDAQ_EB_PORT[42]}" >> $tempFile
-# echo "EventBuilderMain mu2edaq08.fnal.gov ${MU2EARTDAQ_EB_PORT[43]}" >> $tempFile
-# echo "EventBuilderMain mu2edaq08.fnal.gov ${MU2EARTDAQ_EB_PORT[44]}" >> $tempFile
-# echo "EventBuilderMain mu2edaq08.fnal.gov ${MU2EARTDAQ_EB_PORT[45]}" >> $tempFile
-# echo "EventBuilderMain mu2edaq08.fnal.gov ${MU2EARTDAQ_EB_PORT[46]}" >> $tempFile
-# echo "EventBuilderMain mu2edaq08.fnal.gov ${MU2EARTDAQ_EB_PORT[47]}" >> $tempFile
-# echo "EventBuilderMain mu2edaq08.fnal.gov ${MU2EARTDAQ_EB_PORT[48]}" >> $tempFile
-# echo "EventBuilderMain mu2edaq08.fnal.gov ${MU2EARTDAQ_EB_PORT[49]}" >> $tempFile
+echo "EventBuilderMain mu2edaq05.fnal.gov ${MU2EARTDAQ_EB_PORT[10]}" >> $tempFile
+echo "EventBuilderMain mu2edaq05.fnal.gov ${MU2EARTDAQ_EB_PORT[11]}" >> $tempFile
+echo "EventBuilderMain mu2edaq05.fnal.gov ${MU2EARTDAQ_EB_PORT[12]}" >> $tempFile
+echo "EventBuilderMain mu2edaq05.fnal.gov ${MU2EARTDAQ_EB_PORT[13]}" >> $tempFile
+echo "EventBuilderMain mu2edaq05.fnal.gov ${MU2EARTDAQ_EB_PORT[14]}" >> $tempFile
+echo "EventBuilderMain mu2edaq05.fnal.gov ${MU2EARTDAQ_EB_PORT[15]}" >> $tempFile
+echo "EventBuilderMain mu2edaq05.fnal.gov ${MU2EARTDAQ_EB_PORT[16]}" >> $tempFile
+echo "EventBuilderMain mu2edaq05.fnal.gov ${MU2EARTDAQ_EB_PORT[17]}" >> $tempFile
+echo "EventBuilderMain mu2edaq05.fnal.gov ${MU2EARTDAQ_EB_PORT[18]}" >> $tempFile
+echo "EventBuilderMain mu2edaq05.fnal.gov ${MU2EARTDAQ_EB_PORT[19]}" >> $tempFile
+echo "EventBuilderMain mu2edaq06.fnal.gov ${MU2EARTDAQ_EB_PORT[20]}" >> $tempFile
+echo "EventBuilderMain mu2edaq06.fnal.gov ${MU2EARTDAQ_EB_PORT[21]}" >> $tempFile
+echo "EventBuilderMain mu2edaq06.fnal.gov ${MU2EARTDAQ_EB_PORT[22]}" >> $tempFile
+echo "EventBuilderMain mu2edaq06.fnal.gov ${MU2EARTDAQ_EB_PORT[23]}" >> $tempFile
+echo "EventBuilderMain mu2edaq06.fnal.gov ${MU2EARTDAQ_EB_PORT[24]}" >> $tempFile
+echo "EventBuilderMain mu2edaq06.fnal.gov ${MU2EARTDAQ_EB_PORT[25]}" >> $tempFile
+echo "EventBuilderMain mu2edaq06.fnal.gov ${MU2EARTDAQ_EB_PORT[26]}" >> $tempFile
+echo "EventBuilderMain mu2edaq06.fnal.gov ${MU2EARTDAQ_EB_PORT[27]}" >> $tempFile
+echo "EventBuilderMain mu2edaq06.fnal.gov ${MU2EARTDAQ_EB_PORT[28]}" >> $tempFile
+echo "EventBuilderMain mu2edaq06.fnal.gov ${MU2EARTDAQ_EB_PORT[29]}" >> $tempFile
+echo "EventBuilderMain mu2edaq07.fnal.gov ${MU2EARTDAQ_EB_PORT[30]}" >> $tempFile
+echo "EventBuilderMain mu2edaq07.fnal.gov ${MU2EARTDAQ_EB_PORT[31]}" >> $tempFile
+echo "EventBuilderMain mu2edaq07.fnal.gov ${MU2EARTDAQ_EB_PORT[32]}" >> $tempFile
+echo "EventBuilderMain mu2edaq07.fnal.gov ${MU2EARTDAQ_EB_PORT[33]}" >> $tempFile
+echo "EventBuilderMain mu2edaq07.fnal.gov ${MU2EARTDAQ_EB_PORT[34]}" >> $tempFile
+echo "EventBuilderMain mu2edaq07.fnal.gov ${MU2EARTDAQ_EB_PORT[35]}" >> $tempFile
+echo "EventBuilderMain mu2edaq07.fnal.gov ${MU2EARTDAQ_EB_PORT[36]}" >> $tempFile
+echo "EventBuilderMain mu2edaq07.fnal.gov ${MU2EARTDAQ_EB_PORT[37]}" >> $tempFile
+echo "EventBuilderMain mu2edaq07.fnal.gov ${MU2EARTDAQ_EB_PORT[38]}" >> $tempFile
+echo "EventBuilderMain mu2edaq07.fnal.gov ${MU2EARTDAQ_EB_PORT[39]}" >> $tempFile
+echo "EventBuilderMain mu2edaq08.fnal.gov ${MU2EARTDAQ_EB_PORT[40]}" >> $tempFile
+echo "EventBuilderMain mu2edaq08.fnal.gov ${MU2EARTDAQ_EB_PORT[41]}" >> $tempFile
+echo "EventBuilderMain mu2edaq08.fnal.gov ${MU2EARTDAQ_EB_PORT[42]}" >> $tempFile
+echo "EventBuilderMain mu2edaq08.fnal.gov ${MU2EARTDAQ_EB_PORT[43]}" >> $tempFile
+echo "EventBuilderMain mu2edaq08.fnal.gov ${MU2EARTDAQ_EB_PORT[44]}" >> $tempFile
+echo "EventBuilderMain mu2edaq08.fnal.gov ${MU2EARTDAQ_EB_PORT[45]}" >> $tempFile
+echo "EventBuilderMain mu2edaq08.fnal.gov ${MU2EARTDAQ_EB_PORT[46]}" >> $tempFile
+echo "EventBuilderMain mu2edaq08.fnal.gov ${MU2EARTDAQ_EB_PORT[47]}" >> $tempFile
+echo "EventBuilderMain mu2edaq08.fnal.gov ${MU2EARTDAQ_EB_PORT[48]}" >> $tempFile
+echo "EventBuilderMain mu2edaq08.fnal.gov ${MU2EARTDAQ_EB_PORT[49]}" >> $tempFile
 echo "AggregatorMain mu2edaq01.fnal.gov ${MU2EARTDAQ_AG_PORT[0]}" >> $tempFile
 echo "AggregatorMain mu2edaq01.fnal.gov ${MU2EARTDAQ_AG_PORT[1]}" >> $tempFile
 
