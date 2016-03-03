fb99e23fcf99cef6d255ad7404e8b64b9b54e2fa
MU2E <mu2edaq@mu2edaq01.fnal.gov>
2016-03-03 12:30:44 -0600

    Update qualifiers

--- tools/setup_trace.sh
+++ tools/setup_trace.sh
@@ -2,7 +2,7 @@
 #This script sets up TRACE logging for the Mu2e Pilot System.
 
 source /mu2e/ups/setup
-setup TRACE v3_05_00a -qe7
+setup TRACE v3_06_06 -qe9:prof
 
 export TRACE_FILE=/tmp/trace_buffer_$USER
 export TRACE_NAME=MU2EDEV
