aff5d950ecaa5654de4739f970842ec87154edc9
Eric Flumerfelt <eflumerf@fnal.gov>
2018-04-17 13:45:48 -0500

    Compatibility changes for artdaq v3_01_00

--- mu2e-artdaq/Generators/DTCReceiver_generator.cc
+++ mu2e-artdaq/Generators/DTCReceiver_generator.cc
@@ -28,8 +28,7 @@ mu2e::DTCReceiver::DTCReceiver(fhicl::ParameterSet const& ps)
 	  , print_packets_(ps.get<bool>("debug_print", false))
 {
 	// mode_ can still be overridden by environment!
-	auto version = ps.get<std::string>("dtc_firmware_version", "");
-	theInterface_ = new DTCLib::DTC(version, mode_);
+	theInterface_ = new DTCLib::DTC(mode_);
 	theCFO_ = new DTCLib::DTCSoftwareCFO(theInterface_, true);
 	mode_ = theInterface_->ReadSimMode();
 
