4d52d44712c4cf58985e4dc5e2bea0b75229b837
Mu2E <mu2ecrv@mu2edaq08.fnal.gov>
2023-07-21 00:52:20 -0500

    added Simon's changes in the CRVReceiver to the newest version of artdaq-mu2e

--- artdaq-mu2e/ArtModules/OfflineFragmentsDumper_module.cc
+++ artdaq-mu2e/ArtModules/OfflineFragmentsDumper_module.cc
@@ -34,20 +34,20 @@ public:
 private:
 	art::InputTag trkFragmentsTag_;
 	art::InputTag caloFragmentsTag_;
+	art::InputTag crvFragmentsTag_;
 };
 
 mu2e::Mu2eDump::Mu2eDump(fhicl::ParameterSet const& pset)
-	: EDAnalyzer{pset}, trkFragmentsTag_{"daq:trk"}, caloFragmentsTag_{"daq:calo"} {}
-
+	: EDAnalyzer{pset}, trkFragmentsTag_{"daq:trk"}, caloFragmentsTag_{"daq:calo"}, crvFragmentsTag_{"daq:crv"} {}
 void mu2e::Mu2eDump::analyze(art::Event const& e)
 {
 	auto trkFragments = e.getValidHandle<artdaq::Fragments>(trkFragmentsTag_);
 	auto calFragments = e.getValidHandle<artdaq::Fragments>(caloFragmentsTag_);
-
+	auto crvFragments = e.getValidHandle<artdaq::Fragments>(crvFragmentsTag_);
 	std::cout << "Event " << e.event() << " has ";
 	std::cout << trkFragments->size() << " TRK fragments, and ";
 	std::cout << calFragments->size() << " CAL fragments." << std::endl;
-
+	std::cout << crvFragments->size() << " CRV fragments." << std::endl;
 	size_t totalSize = 0;
 	for (size_t idx = 0; idx < trkFragments->size(); ++idx) {
 		auto size = ((*trkFragments)[idx]).size() * sizeof(artdaq::RawDataType);
@@ -59,7 +59,11 @@ void mu2e::Mu2eDump::analyze(art::Event const& e)
 		totalSize += size;
 		std::cout << "\tCAL Fragment " << idx << " has size " << size << std::endl;
 	}
-
+	for (size_t idx = 0; idx < crvFragments->size(); ++idx) {
+		auto size = ((*crvFragments)[idx]).size() * sizeof(artdaq::RawDataType);
+		totalSize += size;
+		std::cout << "\tCRV Fragment " << idx << " has size " << size << std::endl;
+	}
 	std::cout << "\tTotal Size: " << (int)totalSize << " bytes." << std::endl;
 }
 
